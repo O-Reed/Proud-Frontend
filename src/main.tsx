0f0d8eb524c414bf9b60904298fc6d5b792357ce
O-Reed <cooldev0822@gmail.com>
2025-01-22 07:57:47 -0500

    Proud_Frontend_Authentication_OAuth2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Home from './components/Home.tsx'
+import Dashboard from './components/Dashboard.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
