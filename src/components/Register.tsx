0aeb091e49030f32a05b5f3235e594d54063b0bd
O-Reed <cooldev0822@gmail.com>
2025-01-25 14:01:13 -0500

    Proud_Frontend_Register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -5,20 +5,20 @@ import { useState } from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const navigate = useNavigate();
-
+const Register = () => {
+    const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showValidPassword, setShowValidPassword] = useState<boolean>(false);
   const togglePasswordView = () => setShowPassword(!showPassword);
+  const toggleValidPasswordView = () => setShowValidPassword(!showValidPassword);
   
-
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="w-[90%] max-w-sm md:max-w-md lg:max-w-md p-5 bg-gray-900 flex-col flex items-center gap-3 rounded-xl shadow-slate-500 shadow-lg">
         <img src="/logo.png" alt="logo" className="w-12 md:w-14" />
         <h1 className="text-lg md:text-xl font-semibold">Welcome Back</h1>
         <p className="text-xs md:text-sm text-gray-500 text-center">
-          Don't have an account? <span className="text-white" onClick={() => navigate("/register")}>Sign up</span>
+          Don't have an account? <span className="text-white" onClick={() => navigate("/login")}>Sign in</span>
         </p>
 
         <div className="w-full flex flex-col gap-3">
@@ -51,10 +51,30 @@ const Login = () => {
               />
             )}
           </div>
+
+          <div className="w-full flex items-center gap-2 bg-gray-800 p-2 rounded-xl relative">
+            <FaFingerprint />
+            <input
+              type={showValidPassword ? "password" : "text"}
+              placeholder="Password"
+              className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
+            />
+            {showValidPassword ? (
+              <FaRegEyeSlash
+                className="absolute right-5 cursor-pointer"
+                onClick={toggleValidPasswordView}
+              />
+            ) : (
+              <FaRegEye
+                className="absolute right-5 cursor-pointer"
+                onClick={toggleValidPasswordView}
+              />
+            )}
+          </div>
         </div>
 
         <button className="w-full p-2 bg-blue-500 rounded-xl mt-3 hover:bg-blue-600 text-sm md:text-base">
-          Login
+          Register
         </button>
 
         <div className="relative w-full flex items-center justify-center py-3">
@@ -85,4 +105,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.tsx'
 import Login from './components/Login.tsx'
 import Dashboard from './components/Dashboard.tsx'
+import Register from './components/Register.tsx'
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
   {
     path: "/dashboard",
